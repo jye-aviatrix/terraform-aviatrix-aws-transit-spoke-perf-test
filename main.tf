d04ef0116c6474bafa22c4a6a8230f93b0904666
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2023-02-10 23:50:44 -0500

    allow multiple client server pairs to be created

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 module "iperf_client_server" {
+  count = var.number_of_iperf_client_server_pairs
   source                   = "./modules/iperf_client_and_server"
-  client_name              = "iperf_client"
-  server_name              = "iperf_server"
+  client_name              = "iperf_client${count.index}"
+  server_name              = "iperf_server${count.index}"
   client_vpc_id            = module.vpc.vpc_id
   server_vpc_id            = module.vpc.vpc_id
   client_subnet_id         = module.vpc.public_subnets[0]
--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "iperf_client_server_size" {
 variable "key_name" {
   description = "Provide EC2 keypair name"
 }
+
+variable "number_of_iperf_client_server_pairs" {
+  description = "Provide total number of client server pairs !!! Cost will increase when this number increases !!!"
+  default = 2
+}
