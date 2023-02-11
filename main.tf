c3d02fd56b4ff430a049c7d659d201845816bf60
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2023-02-10 23:16:38 -0500

    Add instance vpc and remove roles

--- main.tf
+++ main.tf
@@ -1,50 +1,189 @@
-# resource "aws_instance" "client" {
-#   ami           = "ami-0c55b159cbfafe1f0"
-#   instance_type = "t2.micro"
-#   iam_instance_profile = "${aws_iam_instance_profile.instance_profile.name}"
-#   tags = {
-#     Name = "Client EC2 Instance"
-#   }
+data "aws_ami" "ubuntu_20_04_lts" {
+  most_recent = true
 
-#   user_data = <<EOF
-# #!/bin/bash
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
+}
 
-# yum update -y
-# yum install -y iperf3
+data "http" "ip" {
+  url = "https://ifconfig.me"
+}
 
-# # Get the private IP address of the server
-# server_ip=$(aws ec2 describe-instances --instance-ids $(terraform output server_instance_id) --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
+locals {
+  client_tags = {
+    Name = var.client_name
+  }
+  server_tags = {
+    Name = var.server_name
+  }
 
-# # Start iperf as a client
-# iperf3 -c $server_ip -t 60
-# EOF
-# }
+}
+resource "aws_security_group" "client" {
+  name        = "${var.client_name} Allow inbound SSH from your egress public IP, and all inbound from RFC 1918"
+  description = "${var.client_name} Allow inbound SSH from your egress public IP, and all inbound from RFC 1918"
+  vpc_id      = module.vpc.vpc_id
 
-# resource "aws_instance" "server" {
-#   ami           = "ami-0c55b159cbfafe1f0"
-#   instance_type = "t2.micro"
-#   iam_instance_profile = "${aws_iam_instance_profile.instance_profile.name}"
-#   tags = {
-#     Name = "Server EC2 Instance"
-#   }
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${data.http.ip.response_body}/32"]
+  }
 
-#   user_data = <<EOF
-# #!/bin/bash
+  ingress {
+    description = "10-8"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["10.0.0.0/8"]
+  }
 
-# yum update -y
-# yum install -y iperf3
+  ingress {
+    description = "172-12"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["172.16.0.0/12"]
+  }
 
-# # Start iperf as a server
-# iperf3 -s
-# EOF
-# }
+  ingress {
+    description = "192-16"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+
+  egress {
+    description = "Allow_All_Out_Bound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.client_tags
+}
+
+resource "aws_security_group" "server" {
+  name        = "${var.server_name} Allow inbound SSH from your egress public IP, and all inbound from RFC 1918"
+  description = "${var.server_name} Allow inbound SSH from your egress public IP, and all inbound from RFC 1918"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${data.http.ip.response_body}/32"]
+  }
+
+  ingress {
+    description = "10-8"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["10.0.0.0/8"]
+  }
+
+  ingress {
+    description = "172-12"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["172.16.0.0/12"]
+  }
+
+  ingress {
+    description = "192-16"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+
+  egress {
+    description = "Allow_All_Out_Bound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = local.server_tags
+}
+
+resource "aws_network_interface" "client" {
+  subnet_id       = module.vpc.public_subnets[0]
+  security_groups = [aws_security_group.client.id]
+
+  tags = local.client_tags
+}
+
+resource "aws_network_interface" "server" {
+  subnet_id       = module.vpc.public_subnets[1]
+  security_groups = [aws_security_group.server.id]
+
+  tags = local.server_tags
+}
 
 
 
-# output "client_instance_id" {
-#   value = "${aws_instance.client.id}"
-# }
+resource "aws_instance" "server" {
+  ami           = data.aws_ami.ubuntu_20_04_lts.id
+  instance_type = var.iperf_client_server_size
 
-# output "server_instance_id" {
-#   value = "${aws_instance.server.id}"
-# }
+  network_interface {
+    network_interface_id = aws_network_interface.server.id
+    device_index         = 0
+  }
+
+  key_name = var.key_name
+  tags     = local.server_tags
+
+  user_data = <<EOF
+#!/bin/bash
+
+apt update -y
+apt install -y iperf3
+
+# Start iperf as a server
+while true; do
+    iperf3 -s
+done
+
+EOF
+
+}
+
+
+resource "aws_instance" "client" {
+  ami           = data.aws_ami.ubuntu_20_04_lts.id
+  instance_type = var.iperf_client_server_size
+
+  network_interface {
+    network_interface_id = aws_network_interface.client.id
+    device_index         = 0
+  }
+
+  key_name = var.key_name
+  tags     = local.client_tags
+
+  user_data = <<EOF
+#!/bin/bash
+
+apt update -y
+apt install -y iperf3
+
+# Start iperf as a client
+while true; do
+    iperf3 -c ${aws_network_interface.server.private_ip}
+done
+
+EOF
+
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,19 @@ variable "iperf_instance_policy_name" {
 variable "iperf_instance_profile_name" {
   default = "iperf_instance_profile"
 }
+
+variable "client_name" {
+    default = "iperf_client"  
+}
+
+variable "server_name" {
+  default = "iperf_server"
+}
+
+variable "iperf_client_server_size" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  description = "Provide EC2 keypair name"
+}
